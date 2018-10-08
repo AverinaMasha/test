1c1fbdd594c0d8f202abe0828bbedfa4ddb232b9
Аверина Мария Дмитриевна <Averina@WUD.lan>
2018-10-08 17:31:19 +0300

    HElloWorld1 itog

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ class Hello
 public static void main(String args[ ]) 
       { 
 System.out.println( "A" ); 
-System.out.println( "Hello world" ); 
+System.out.println( "Hello world1" ); 
 System.out.println( "B" ); 
 
        } 
