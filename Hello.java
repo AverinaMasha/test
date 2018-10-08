207111b7d6e09d1478c55c0f259a474e8e5aa116
Аверина Мария Дмитриевна <Averina@WUD.lan>
2018-10-08 16:24:01 +0300

    New project

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.io.*; 
+// This is a simple program called HelloWorld.java 
+class Hello
+   { 
+public static void main(String args[ ]) 
+      { 
+System.out.println( "Hello world" ); 
+
+       } 
+   }
